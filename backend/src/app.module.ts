618c63ebbef0379e8780e36596439f37b00d8f68
Lidiya Mergiya <mergiyalidiya@gmail.com>
2026-09-23 22:03:58 +0300

    feat: add admin module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { RecommendationsModule } from './recommendations/recommendations.module';
 import { AssessmentsModule } from './assessments/assessments.module';
 import { VoiceModule } from './voice/voice.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [RecommendationsModule, AssessmentsModule, VoiceModule]
+  imports: [RecommendationsModule, AssessmentsModule, VoiceModule, AdminModule]
 })
 export class AppModule {}
