fe29ff38af58953a9b983a3efe5f0cc9c03abcf8
Lidiya Mergiya <mergiyalidiya@gmail.com>
2026-09-23 20:52:55 +0300

    feat: add voice module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RecommendationsModule } from './recommendations/recommendations.module';
 import { AssessmentsModule } from './assessments/assessments.module';
+import { VoiceModule } from './voice/voice.module';
 
 @Module({
-  imports: [RecommendationsModule, AssessmentsModule]
+  imports: [RecommendationsModule, AssessmentsModule, VoiceModule]
 })
 export class AppModule {}
