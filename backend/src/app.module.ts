8569d06362589d16e320352b70837eafed29a0da
Lidiya Mergiya <mergiyalidiya@gmail.com>
2026-09-23 20:47:26 +0300

    feat: add assessments module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { RecommendationsModule } from './recommendations/recommendations.module';
+import { AssessmentsModule } from './assessments/assessments.module';
 
 @Module({
-  imports: [RecommendationsModule]
+  imports: [RecommendationsModule, AssessmentsModule]
 })
 export class AppModule {}
